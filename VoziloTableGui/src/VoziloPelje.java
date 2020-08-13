edf20b875386dc855017d7ba78a4dff0c1506c48
Pixie <Pixie@DESKTOP-VDTHU9P>
2020-08-13 13:16:54 +0200

    pozabljeno

---
+++ VoziloTableGui/src/VoziloPelje.java
@@ -0,0 +1,12 @@
+/* Vaja: 34
+    Avtor: Matjaž Kic
+    Naloga: Izdelajte vmesnik ali abstraktni razred, ki ga boste nato smiselno implementirali v vašem osnovnem razredu.
+
+*/
+
+
+public interface VoziloPelje
+{
+    public int pelji(int d) throws Exception;
+
+}
